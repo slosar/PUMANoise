2735b287e5c8fb5b333c3a51dc218f02e1c64e3f
Simon Foreman <sforeman@cita.utoronto.ca>
2019-11-08 22:07:58 -0500

    fix lower threshold of res in nofl(x) to always use numpy ndarray

--- pumanoise.py
+++ pumanoise.py
@@ -35,14 +35,14 @@ class RadioTelescope:
             a,b,B,C,D=0.56981864, -0.52741196,  0.8358006 ,  1.66354748,  7.31776875
         xn=x/(self.Nside*self.D)
         n0=(self.Nside/self.D)**2
-        res=n0*(a+b*xn)/(1+B*xn**C)*np.exp(-(xn)**D)
+        res=np.array(n0*(a+b*xn)/(1+B*xn**C)*np.exp(-(xn)**D))
         if (type(res)==np.ndarray):
             res[res<1e-10]=1e-10
-        if (res<1e-10):
-            res=1e-10
+        # if (res<1e-10):
+        #     res=1e-10
         return res
-        
-        
+
+
     def PNoise(self,z,kperp):
         """ Thermal noise power in Mpc^3 """
         lam=0.21*(1+z)
@@ -77,7 +77,7 @@ class RadioTelescope:
 
     def bias(self,z):
         return castorinaBias(z)
-    
+
     def Tsky(self,f):
         #return (f/100.)**(-2.4)*2000+2.7 ## from CVFisher
         return 25.*(f/400.)**(-2.75) +2.75
@@ -94,9 +94,9 @@ class RadioTelescope:
         Ohi=4e-4*(1+z)**0.6
         Tb=188e-3*self.C['h']/Ez*Ohi*(1+z)**2
         return Tb
-    
 
-        
+
+
     def cutWedge(self, noise, kperp, kpar, z, NW=3.0):
         r=ccl.comoving_radial_distance(self.C,1/(1.+z))
         H=self.C['H0']*ccl.h_over_h0(self.C,1./(1.+z))
@@ -114,14 +114,14 @@ class RadioTelescope:
         teff=np.sqrt(FOV)/(2*np.pi*np.cos(30/180*np.pi))*24*3600 ## 30 deg south
         teffchime=(lam/20)/(2*np.pi*np.cos(50/180*np.pi))*24*3600 ## 50 deg north
         print ("Acoll*np.sqrt(teff*bandwidth*1e6)",Acoll * np.sqrt(2*teff*bandwidth*1e6))
-        
+
         Tsys = self.Tsky(freq)+self.Tscope
         onesigma= 2 * kB * Tsys /  ( Acoll * np.sqrt(2*teff*bandwidth*1e6)) / 1e-26 ## to Jy
         print ("Tsys",Tsys)
         print ("teff=",teff,teffchime)
         print ("CHIME:", 10* 2 *kB * Tsys / (0.7*80*100*np.sqrt(2*teffchime * 400e6))/1e-26)
         return onesigma
-    
+
 
 
 class PUMA(RadioTelescope):
@@ -133,5 +133,3 @@ class PUMAPetite(RadioTelescope):
     def __init__ (self,C):
         RadioTelescope.__init__(self,C,Nside=100, D=6, tint=5/4, fsky=0.5, effic=0.7,
                                 Tampl=50., Tground=300., omtcoupling=0.9, skycoupling=0.9, hexpack=True)
-
-
